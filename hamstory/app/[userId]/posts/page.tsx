360a5cbafd0b32ad0b809c03e27cd33750f11204
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-05-29 01:24:36 +0900

    Feat. API 요청해보기

--- hamstory/app/[userId]/posts/page.tsx
+++ hamstory/app/[userId]/posts/page.tsx
@@ -1,3 +1,14 @@
-export default function PostListPage() {
+export default async function PostListPage() {
+  const res = await fetch("http://localhost:3000/api", {
+    method: "GET",
+    headers: {
+      "X-ID": "test",
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await res.json();
+
+  console.log(data);
+
   return <div>Post List Page.</div>;
 }
