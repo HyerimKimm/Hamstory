62dc45739f1878c3c911a1ee5bb0ff76a0fc370a
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-06-03 23:55:40 +0900

    Feat. sass, framer-motion, mongodb 설치

---
+++ hamstory/app/api/user/signup/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import { MongoClient } from "mongodb";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  const url =
+    "mongodb+srv://sue05124:hlkim980103@hamstory.bgdlfqq.mongodb.net/?retryWrites=true&w=majority&appName=Hamstory";
+
+  const client = new MongoClient(url);
+
+  await client.connect();
+  console.log("Connected successfully to server");
+
+  const db = client.db("hamstory");
+
+  db.collection("users").insertOne({
+    nickname: body.nickname,
+    email: body.email,
+    password: body.password,
+  });
+
+  client.close();
+
+  return NextResponse.json({
+    message: "성공",
+  });
+}
--- hamstory/app/page.tsx
+++ hamstory/app/page.tsx
@@ -1,3 +1,48 @@
+"use client";
+
+import { useRef } from "react";
+
 export default function Home() {
-  return <div>Root Page.</div>;
+  const nicknameInputRef = useRef<HTMLInputElement>(null);
+  const emailInputRef = useRef<HTMLInputElement>(null);
+  const passwordInputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (
+      nicknameInputRef.current &&
+      emailInputRef.current &&
+      passwordInputRef.current
+    ) {
+      fetch(`/api/user/signup`, {
+        method: "POST",
+        body: JSON.stringify({
+          nickname: nicknameInputRef.current.value,
+          email: emailInputRef.current.value,
+          password: passwordInputRef.current.value,
+        }),
+      });
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      Root Page.
+      <input
+        ref={nicknameInputRef}
+        type="text"
+        placeholder="닉네임을 입력해 주세요."
+      />
+      <input
+        ref={emailInputRef}
+        type="email"
+        placeholder="이메일을 입력해 주세요."
+      />
+      <input
+        ref={passwordInputRef}
+        type="password"
+        placeholder="비밀번호를 입력해 주세요."
+      />
+      <button>제출</button>
+    </form>
+  );
 }
