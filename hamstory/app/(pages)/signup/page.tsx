b48e3f0eb362bf8f3b838f091626243d8a6658b2
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-08-11 23:08:12 +0900

    Feat. signup 구조 수정

--- hamstory/app/(pages)/signup/page.tsx
+++ hamstory/app/(pages)/signup/page.tsx
@@ -24,25 +24,5 @@ export default function SignUpPage() {
       });
     }
   };
-  return (
-    <form onSubmit={handleSubmit}>
-      Root Page.
-      <input
-        ref={nicknameInputRef}
-        type="text"
-        placeholder="닉네임을 입력해 주세요."
-      />
-      <input
-        ref={emailInputRef}
-        type="email"
-        placeholder="이메일을 입력해 주세요."
-      />
-      <input
-        ref={passwordInputRef}
-        type="password"
-        placeholder="비밀번호를 입력해 주세요."
-      />
-      <button>제출</button>
-    </form>
-  );
+  return <div>Signup Page.</div>;
 }
---
+++ hamstory/app/signup/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useRef } from "react";
+
+export default function SignUpPage() {
+  const nicknameInputRef = useRef<HTMLInputElement>(null);
+  const emailInputRef = useRef<HTMLInputElement>(null);
+  const passwordInputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (
+      nicknameInputRef.current &&
+      emailInputRef.current &&
+      passwordInputRef.current
+    ) {
+      fetch(`/api/user/signup`, {
+        method: "POST",
+        body: JSON.stringify({
+          nickname: nicknameInputRef.current.value,
+          email: emailInputRef.current.value,
+          password: passwordInputRef.current.value,
+        }),
+      });
+    }
+  };
+  return (
+    <div>
+      <h1>회원가입 페이지</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          ref={nicknameInputRef}
+          type="text"
+          placeholder="닉네임을 입력해 주세요."
+        />
+        <input
+          ref={emailInputRef}
+          type="email"
+          placeholder="이메일을 입력해 주세요."
+        />
+        <input
+          ref={passwordInputRef}
+          type="password"
+          placeholder="비밀번호를 입력해 주세요."
+        />
+        <button>제출</button>
+      </form>
+    </div>
+  );
+}
