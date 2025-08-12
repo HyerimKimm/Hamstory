7f0746be3d16d6ffd7870e4c857bfdb53d7251d2
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-08-12 23:00:20 +0900

    Feat. 회원가입 폼 추가

--- hamstory/app/(pages)/@modal/(.)signup/page.tsx
+++ hamstory/app/(pages)/@modal/(.)signup/page.tsx
@@ -1,3 +1,50 @@
+import ModalHeader from "@/components/modal/ModalHeader";
+import ModalLayout from "@/components/modal/ModalLayout";
+
+import styles from "./page.module.scss";
+
 export default function SignupModal() {
-  return <div>회원가입 모달</div>;
+  return (
+    <ModalLayout width={330}>
+      <ModalHeader title="회원가입" />
+      <form className={styles.content_wrap}>
+        <div className={styles.content_input_wrap}>
+          <label className={styles.label}>닉네임</label>
+          <input
+            name="nickname"
+            type="text"
+            className={styles.content_input}
+            placeholder="닉네임을 입력해 주세요."
+            required
+          />
+        </div>
+
+        <div className={styles.content_input_wrap}>
+          <label className={styles.label}>이메일</label>
+          <input
+            name="email"
+            type="email"
+            className={styles.content_input}
+            placeholder="이메일을 입력해 주세요."
+            required
+          />
+        </div>
+
+        <div className={styles.content_input_wrap}>
+          <label className={styles.label}>비밀번호</label>
+          <input
+            name="password"
+            type="password"
+            className={styles.content_input}
+            placeholder="비밀번호를 입력해 주세요."
+            required
+          />
+        </div>
+
+        <button type="submit" className={styles.submit_button}>
+          회원가입
+        </button>
+      </form>
+    </ModalLayout>
+  );
 }
