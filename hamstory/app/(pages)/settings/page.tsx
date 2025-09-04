f69e53e3f7c037884ae600e6468dc4a004478a22
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-09-04 22:59:50 +0900

    Feat. verifyAuth 사용해서 /settings 에서 로그인 안한 상태이면 로그인 페이지로 리다이렉트 시킴

--- hamstory/app/(pages)/settings/page.tsx
+++ hamstory/app/(pages)/settings/page.tsx
@@ -1,3 +1,13 @@
-export default function SettingsPage() {
+import { redirect } from "next/navigation";
+
+import { verifyAuth } from "@/lib/user/auth";
+
+export default async function SettingsPage() {
+  const result = await verifyAuth();
+
+  if (!result.success) {
+    redirect("/login");
+  }
+
   return <div>Settings Page.</div>;
 }
